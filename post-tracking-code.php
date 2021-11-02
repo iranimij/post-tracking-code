91525018738483606bc98adb446ad6ea1b332416
iranimij1 <iman@artbees.net>
2021-11-02 18:05:18 +0330

    Update url

--- post-tracking-code.php
+++ post-tracking-code.php
@@ -13,7 +13,6 @@
  * Author:            Iman Heydari
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Author URI:        http://www.iranimij.com
  * Text Domain:       ptc
  * Domain Path:       /languages
  */
